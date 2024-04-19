788df23e410903b42538340d97738ac0b3b03422
sssadboy <115662109+Lleanin@users.noreply.github.com>
2024-04-19 17:49:24 +0300

    Integrate livereload

--- render_website.py
+++ render_website.py
@@ -1,20 +1,27 @@
 import json
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from livereload import Server, shell
 
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
-template = env.get_template('template.html')
+def on_reload(): # Рендерит template.html в index.html и сохраняет новый файл.
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    template = env.get_template('template.html') # Python-скрипт находит HTML-шаблон.
 
-with open("books.json", encoding="utf8") as my_file:
-    books_json = my_file.read()
-books = json.loads(books_json)
+    with open("books.json", encoding="utf8") as my_file:
+        books_json = my_file.read()
+    books = json.loads(books_json) # достаю информацию о книгах.
 
-rendered_page = template.render(  
-    books = books
-)
+    rendered_page = template.render(  # рендеринг template.html.
+        books = books
+    )
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page) 
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page) # Сохранение файла.
+
+
+server = Server() # Создание сервера
+server.watch('template.html',on_reload) # Команда которая следит за изменениями в файлах и после каждого изменения запускает консольную программу.
+server.serve(root='.') # Запуск сервера.
